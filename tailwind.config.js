85a8479345e7c3ec8bdf4f5d0f9253cd0b2dc25b
dinesh24murali <dinesh24murali@gmail.com>
2022-01-22 13:43:24 +0530

    Add: tailwind css for React script 5;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       primary: '#202225',
