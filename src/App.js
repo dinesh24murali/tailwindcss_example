8508deb08c10e5322c2996b99f151b9b71afbed2
dinesh24murali <dinesh24murali@gmail.com>
2022-01-22 13:34:51 +0530

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
-// import NavBar from './NavBar';
 import Form from './Form/Form';
-
-import './App.css';
 
 function App() {
   return (<div className="flex">
-    {/* <NavBar /> */}
-    {/* <div className={styles.title}>Title</div>
-    <div className={styles.sub_title}>Sub title</div> */}
     <Form />
   </div>
   );
