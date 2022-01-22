30968f06a9c6fcca3dde7295eedc11f6d9c57abc
dinesh24murali <dinesh24murali@gmail.com>
2022-01-22 16:50:43 +0530

    Fix: updated tailwind configs to support version 3.0;

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -6,24 +6,17 @@ import Button from './Button';
 export default function Form() {
 
     const [userName, setUserName] = useState('');
-    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     return <div className="container m-auto mt-28">
         <div className="p-10 rounded-xl shadow-md m-auto flex w-6/12 flex-col items-center">
+            <div className="mb-5 text-primary">Registration form</div>
             <Input
                 type="text"
                 className="mb-2"
                 onChange={({ target }) => setUserName(target.value)}
                 value={userName}
                 placeholder="user name"
-            />
-            <Input
-                type="email"
-                className="mb-2"
-                onChange={({ target }) => setEmail(target.value)}
-                value={email}
-                placeholder="email"
             />
             <Input
                 type="password"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -8,29 +6,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
-      primary: '#202225',
-      secondary: '#5865f2',
-      gray: colors.trueGray,
-      gray: {
-        900: '#202225',
-        800: '#2f3136',
-        700: '#36393f',
-        600: '#4f545c',
-        400: '#d4d7dc',
-        300: '#e3e5e8',
-        200: '#ebedef',
-        100: '#f2f3f5',
-      }
+      colors: ({ colors }) => ({
+        primary: '#0d6efd',
+        secondary: '#6c757d',
+        gray: colors.trueGray,
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        }
+      })
     },
   },
-  variants: {
-    extend: {
-      display: ['group-hover'],
-      scale: ['active', 'group-hover'],
-      borderColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'focus-visible'],
-      borderOpacity: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'focus-visible'],
-      borderWidth: ['responsive'],
-    },
-  },
+  variants: {},
   plugins: [],
 }
